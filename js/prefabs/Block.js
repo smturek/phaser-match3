91009ce5e4733b1f5b2057829d034961da556d44
smturek <smturek@gmail.com>
2015-09-21 07:47:31 -0600

    method for creating block visuals

--- js/prefabs/Block.js
+++ js/prefabs/Block.js
@@ -5,9 +5,19 @@ Match3.Block = function(state, x, y, data) {
 
     this.game = state.game;
     this.state = state;
+    this.row = data.row;
+    this.col = data.col;
 
     this.anchor.setTo(0.5);
 };
 
 Match3.Block.prototype = Object.create(Phaser.Sprite.prototype);
 Match3.Block.prototype.constructor = Match3.Block;
+
+Match3.Block.prototype.reset = function(x, y, data) {
+    Phaser.Sprite.prototype.reset.call(this, x, y);
+    this.loadTexture(data.asset);
+
+    this.row = data.row;
+    this.col = data.col;
+};
